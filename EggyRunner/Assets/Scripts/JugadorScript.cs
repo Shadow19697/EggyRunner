33b926989a4af3f5ade4b09b86c535d4ceda82a1
Martín Alejandro Campero <mcampero@onebittech.com>
2021-06-28 09:48:25 -0300

    Update 2021.6.28

--- EggyRunner/Assets/Scripts/JugadorScript.cs
+++ EggyRunner/Assets/Scripts/JugadorScript.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 public class JugadorScript : MonoBehaviour
 {
     public int PotSalto;
-    public int VelMovimiento;
+    public float VelMovimiento;
     bool EnPiso = false;
+    private AudioSource Sonido;
+    public AudioClip Salto;
+    public float SegIncremento;
+    float TiempoActual;
+
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        Sonido = GetComponent<AudioSource>();
+        TiempoActual = SegIncremento * 300;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("space") && EnPiso)
-        {
+    void Update(){
+        TiempoActual -= 1f;
+        if (Input.GetKeyDown("space") && EnPiso){
+            Sonido.PlayOneShot(Salto, 1f);
+            EnPiso = false;
             this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, PotSalto));
         }
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(VelMovimiento,
             this.GetComponent<Rigidbody2D>().velocity.y);
+        if(TiempoActual < 0){
+            VelMovimiento += 1;
+            TiempoActual = SegIncremento * 300;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D C1){
+        EnPiso = true;
+        if (C1.collider.gameObject.tag == "Obstacle"){
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D C1){
+        EnPiso = true;
+        if (C1.gameObject.tag == "Obstacle"){
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision){
         EnPiso = true;
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        EnPiso = false;
-    }
+
 }
